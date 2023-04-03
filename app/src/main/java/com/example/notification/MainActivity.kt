727d592911ce5daa9369686f3b3e04bcf2ebff20
Ankit <ankit_1302@outlook.com>
2023-04-03 21:32:19 +0530

    Permission problem was there but it shows Local notification

--- app/src/main/java/com/example/notification/MainActivity.kt
+++ app/src/main/java/com/example/notification/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.example.notification
 
+import android.app.NotificationManager
 import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.LocalAbsoluteTonalElevation
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
 import com.example.notification.ui.theme.NotificationTheme
 import java.util.jar.Manifest
@@ -33,25 +41,52 @@ class MainActivity : ComponentActivity() {
                                 android.Manifest.permission.POST_NOTIFICATIONS
                             ) == PackageManager.PERMISSION_GRANTED
                         )
-                    } else {
-                        TODO("VERSION.SDK_INT < TIRAMISU")
+                    } else mutableStateOf(true)
+                }
+                val permissionLauncher = rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.RequestPermission(),
+                    onResult = { isGranted -> hasNotificationPermission = isGranted }
+                )
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+
+                    Button(onClick = {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                        permissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
                     }
 
+                    }) {
+                        Text(text = "Request Permission")
+                    }
+                    Button(onClick = {
+                    if (hasNotificationPermission){
+                        showNotification()
+                    }
+                    }) {
+                        Text(text = "Show notification")
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+    private fun showNotification() {
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    NotificationTheme {
-        Greeting("Android")
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notification = NotificationCompat.Builder(applicationContext, "channel_id")
+            .setContentText("This is some content text")
+            .setContentText("Hello World")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .build()
+
+        notificationManager.notify(1, notification)
     }
+
 }
+
